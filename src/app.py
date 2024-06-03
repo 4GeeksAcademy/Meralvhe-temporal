8a6d55f4ba6d2ce6ce73832fbfce549a82e92c70
Mery Álvarez <159366647+Meryalvhe@users.noreply.github.com>
2024-06-03 17:28:14 +0000

    feat

--- src/app.py
+++ src/app.py
@@ -25,13 +25,40 @@ def sitemap():
     return generate_sitemap(app)
 
 
-@app.route('/members', methods=['GET'])
+@app.route('/members', methods=['GET', 'POST'])
 def handle_hello():
+    response_body= {}
     # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body["hello"] = "world"
-    response_body[ "family"] = members
-    return jsonify(response_body), 200
+    if request.method == 'GET':
+        members = jackson_family.get_all_members()
+        response_body["hello"] = "world"
+        response_body[ "family"] = members
+        return jsonify(response_body), 200
+    if request.method == 'POST':
+        data = request.json
+        results = jackson_family.add_member(data)
+        response_body['message'] = 'El endpoint funciona'
+        response_body['results'] = results
+        return response_body, 200
+
+
+@app.route('/members/<int:id>', methods=['GET', 'DELETE'])
+def handle_members(id):
+    response_body = {}
+    if request.method == 'GET':
+        result = jackson_family.get_member(id)
+        if result: 
+            response_body['message'] = 'Usuario encontrado'
+            response_body['results'] = result
+            return response_body, 200
+        response_body['message'] = 'Usuario no encontrado'
+        response_body['results'] = {}
+        return response_body, 404
+    if request.method == 'DELETE':
+        result = jackson_family.delete_member(id)
+        response_body["message"] = 'Usuario eliminado'
+        response_body['results'] = result
+        return response_body, 200
 
 
 if __name__ == '__main__':
